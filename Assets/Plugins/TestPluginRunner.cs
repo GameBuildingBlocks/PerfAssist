63914db94a4762438207bd46a707a8b6c867a23e
Gu Lu <gemfeeling@hotmail.com>
2016-09-22 16:01:20 +0800

    实现插件下的协程追踪（新增类 <PluginName>.CoroutineForwarder）

---
+++ Assets/Plugins/TestPluginRunner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPluginRunner : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+        PluginName.CoroutineForwarder.InvokeStart(this, Co01_Plugin_NULL());
+        PluginName.CoroutineForwarder.InvokeStart(this, "Co02_Plugin_EOF_ByName");
+        PluginName.CoroutineForwarder.InvokeStart(this, "Co03_Plugin_EOF_ByNameWithArg", 0.369f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public IEnumerator Co01_Plugin_NULL()
+    {
+        while (true)
+        {
+            Debug.LogFormat("Co01_Plugin_NULL.tick: {0}", Time.time);
+            yield return null;
+        }
+    }
+    public IEnumerator Co02_Plugin_EOF_ByName()
+    {
+        while (true)
+        {
+            Debug.LogFormat("Co02_Plugin_EOF_ByName.tick: {0}", Time.time);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    public IEnumerator Co03_Plugin_EOF_ByNameWithArg(float arg)
+    {
+        while (true)
+        {
+            Debug.LogFormat("Co03_Plugin_EOF_ByNameWithArg.tick: {0} - {1}", Time.time, arg);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
--- Assets/SampleRunner.cs
+++ Assets/SampleRunner.cs
@@ -9,8 +9,11 @@ public class SampleRunner : MonoBehaviour {
         RuntimeCoroutineTracker.EnableTrackingSystemProgrammatically();
         StartCoroutine(RuntimeCoroutineTracker.DefaultStatsReportCoroutine());
 
-        var spawner = gameObject.GetComponent<CoroutineSpawner>() as CoroutineSpawner;
+        TestPluginRunner pluginRunner = gameObject.AddComponent<TestPluginRunner>();
+        PluginName.CoroutineForwarder.InvokeStart_IEnumerator = RuntimeCoroutineTracker.InvokeStart;
+        PluginName.CoroutineForwarder.InvokeStart_String = RuntimeCoroutineTracker.InvokeStart;
 
+        CoroutineSpawner spawner = gameObject.AddComponent<CoroutineSpawner>();
         RuntimeCoroutineTracker.InvokeStart(spawner, "Co01_WaitForSeconds");
         RuntimeCoroutineTracker.InvokeStart(spawner, "Co02_PerFrame_NULL");
         RuntimeCoroutineTracker.InvokeStart(spawner, "Co03_PerFrame_EOF");
