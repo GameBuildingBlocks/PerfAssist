52bd0014bef72c849a07bae8dcaa91637ec47ac8
Gu Lu <gemfeeling@hotmail.com>
2016-10-18 10:56:35 +0800

    initial commits

---
+++ Editor/PAEditorUtil.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PAEditorUtil
+{
+    public static void DrawLabel(string content, GUIStyle style)
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        EditorGUILayout.LabelField(content, style, GUILayout.Width(style.CalcSize(new GUIContent(content)).x + 3));
+        EditorGUILayout.EndHorizontal();
+    }
+}
