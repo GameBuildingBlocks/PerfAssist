d357425af7ac2d867c2708d205012b7710da124b
Gu Lu <gemfeeling@hotmail.com>
2016-11-10 15:34:58 +0800

    remove temporarily

--- Assets/PerfAssist/PA_Common/Editor/PAEditorConst.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PAEditorConst
-{
-    public readonly static Color SelectionColor = (Color)new Color32(62, 95, 150, 255);
-    public readonly static Color SelectionColorDark = (Color)new Color32(62, 95, 150, 128);
-}
