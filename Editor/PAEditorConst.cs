174b1464c8b7d99764a0634764b68f9dde49e663
Gu Lu <gemfeeling@hotmail.com>
2016-11-25 10:42:18 +0800

    add const colors

--- Editor/PAEditorConst.cs
+++ Editor/PAEditorConst.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PAEditorConst
 {
+    public readonly static Color TitleColor = (Color)new Color32(38, 158, 111, 255);        // basically green
+    public readonly static Color TitleColorSelected = (Color)new Color32(19, 80, 60, 255);  // dark green
+
     public readonly static Color SelectionColor = (Color)new Color32(62, 95, 150, 255);
     public readonly static Color SelectionColorDark = (Color)new Color32(62, 95, 150, 128);
     public readonly static string BytesFormatter = "<fmt_bytes>";
