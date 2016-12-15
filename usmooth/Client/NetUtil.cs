a2a0eecea4493ab5f86922b950d01d127bca635f
Gu Lu <gemfeeling@hotmail.com>
2016-12-15 17:25:54 +0800

    add client code

---
+++ usmooth/Client/NetUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate void NetLogHandler(string fmt, params object[] args);
+
+public static class NetUtil
+{
+    public static NetLogHandler LogHandler { get; set; }
+    public static NetLogHandler LogErrorHandler { get; set; }
+
+    public static void Log(string fmt, params object[] args)
+    {
+        if (LogHandler != null)
+            LogHandler(fmt, args);
+    }
+
+    public static void LogError(string fmt, params object[] args)
+    {
+        if (LogErrorHandler != null)
+            LogErrorHandler(fmt, args);
+    }
+}
+
