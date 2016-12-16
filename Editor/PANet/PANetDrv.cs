68a21b1d8336ab3ad465f16c6a7efeb3cf6051f6
Gu Lu <gemfeeling@hotmail.com>
2016-12-16 10:15:49 +0800

    add default net drv impl

---
+++ Editor/PANet/PANetDrv.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PANetDrv : IDisposable
+{
+    public static PANetDrv Instance = new PANetDrv();
+
+    public PANetDrv()
+    {
+        NetUtil.LogHandler = Debug.LogFormat;
+        NetUtil.LogErrorHandler = Debug.LogErrorFormat;
+
+        NetManager.Instance = new NetManager();
+        NetManager.Instance.Client.RegisterCmdHandler(eNetCmd.SV_App_Logging, Handle_ServerLogging);
+    }
+
+    private bool Handle_ServerLogging(eNetCmd cmd, UsCmd c)
+    {
+        UsLogPacket pkt = new UsLogPacket(c);
+
+        string logTypeStr = "";
+        switch (pkt.LogType)
+        {
+            case UsLogType.Error:
+            case UsLogType.Exception:
+            case UsLogType.Assert:
+            case UsLogType.Warning:
+                logTypeStr = string.Format("{1}", pkt.LogType);
+                break;
+
+            case UsLogType.Log:
+            default:
+                break;
+        }
+
+        string timeStr = string.Format("{1:0.00}({2})", pkt.RealtimeSinceStartup, pkt.SeqID);
+
+        string ret = string.Format("{0} {1} <color=white>{2}</color>", timeStr, logTypeStr, pkt.Content);
+
+        if (!string.IsNullOrEmpty(pkt.Callstack))
+        {
+            ret += string.Format("\n<color=gray>{0}</color>", pkt.Callstack);
+        }
+
+        Debug.Log(ret);
+
+        return true;
+    }
+
+    public void Dispose()
+    {
+        NetManager.Instance.Dispose();
+    }
+}
