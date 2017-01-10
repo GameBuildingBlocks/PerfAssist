84f3dd394f9db5f940839899042dbd024417df01
thlx <48083409@qq.com>
2017-01-10 13:28:04 +0800

    realize ShaderComponnentScanner

---
+++ ResourceTrackerConst.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using LitJson;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ResourceTrackerConst
+{
+    public static string shaderPropertyNameJsonPath =
+    Path.Combine(Path.Combine(Application.persistentDataPath, "TestTools"), "ShaderPropertyNameRecord.json");
+    public static char shaderPropertyNameJsonToken = '$';
+}
