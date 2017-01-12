bb1fff496cad2aad892d8c1164f858ed8e414cad
Gu Lu <gemfeeling@hotmail.com>
2017-01-12 15:43:04 +0800

    activities moved into Plugins

--- RuntimeCoroutineStats.cs
+++ RuntimeCoroutineStats.cs
@@ -3,48 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-
-public class CoroutineActivity
-{
-    public int seqID;
-    public float timestamp;
-    public int curFrame;
-    public string typeName; // 仅作为方便解析json使用
-
-    public CoroutineActivity(int id)
-    {
-        seqID = id;
-        timestamp = Time.realtimeSinceStartup;
-        typeName = GetType().Name.Substring("Coroutine".Length);
-    }
-}
-
-public class CoroutineCreation : CoroutineActivity
-{
-    public string mangledName;
-    public string stacktrace;
-
-    public CoroutineCreation(int seq) : base(seq)
-    {
-    }
-}
-
-public class CoroutineExecution : CoroutineActivity
-{
-    public float timeConsumed;
-
-    public CoroutineExecution(int seq) : base(seq)
-    {
-    }
-}
-
-public class CoroutineTermination : CoroutineActivity
-{
-    public CoroutineTermination(int seq) : base(seq)
-    {
-    }
-}
-
 public delegate void OnCoStatsBroadcast(List<CoroutineActivity> activities);
 public delegate void OnCoStatsAnalyzer2File(List<CoroutineActivity> activities);
 
