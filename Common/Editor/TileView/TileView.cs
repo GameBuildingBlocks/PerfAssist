f0da16a917d5432b0e756833757a2cba12af15d2
gulu_office <gemfeeling@hotmail.com>
2017-09-04 16:03:06 +0800

    add TileView & TextureInfoViewer

---
+++ Common/Editor/TileView/TileView.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+public delegate void TileSelectionHandler(object selected, int tile);
+
+public partial class TileView : IDisposable
+{
+    public event TileSelectionHandler OnSelected;
+
+    public int ColumnCount = 4;
+
+    public TileViewAppr Appearance { get { return _appearance; } }
+
+    public TileView(EditorWindow hostWindow)
+    {
+        m_hostWindow = hostWindow;
+    }
+
+    public void Dispose()
+    {
+
+    }
+
+    //public bool AddColumn(string colDataPropertyName, string colTitleText, float widthByPercent, TextAnchor alignment = TextAnchor.MiddleCenter, string fmt = "")
+    //{
+    //    TableViewColDesc desc = new TableViewColDesc();
+    //    desc.PropertyName = colDataPropertyName;
+    //    desc.TitleText = colTitleText;
+    //    desc.Alignment = alignment;
+    //    desc.WidthInPercent = widthByPercent;
+    //    desc.Format = string.IsNullOrEmpty(fmt) ? null : fmt;
+    //    desc.FieldInfo = m_itemType.GetField(desc.PropertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
+    //    if (desc.FieldInfo == null)
+    //    {
+    //        Debug.LogWarningFormat("Field '{0}' accessing failed.", desc.PropertyName);
+    //        return false;
+    //    }
+
+    //    m_descArray.Add(desc);
+    //    return true;
+    //}
+
+    public void RefreshData(List<object> entries)
+    {
+        m_objects.Clear();
+
+        if (entries != null && entries.Count > 0)
+        {
+            m_objects.AddRange(entries);
+
+            //SortData();
+        }
+    }
+
+    public void Draw(Rect area)
+    {
+        GUILayout.BeginArea(area);
+        _scrollPos = GUILayout.BeginScrollView(_scrollPos, GUIStyle.none, GUI.skin.verticalScrollbar);
+        //Debug.LogFormat("scroll pos: {0:0.00}, {1:0.00}", _scrollPos.x, _scrollPos.y);
+        {
+            float lineHeight = area.width / ColumnCount;
+
+            GUIStyle s = new GUIStyle();
+            s.fixedHeight = lineHeight * (m_objects.Count / ColumnCount + 2);
+            s.stretchWidth = true;
+            Rect r = EditorGUILayout.BeginVertical(s);
+            {
+                // this silly line (empty label) is required by Unity to ensure the scroll bar appear as expected.
+                PAEditorUtil.DrawLabel("", _appearance.Style_Line);
+
+                // these first/last calculations are for smart clipping 
+                int firstLine = Mathf.Max((int)(_scrollPos.y / lineHeight) - 1, 0);
+                int shownLineCount = (int)(area.height / lineHeight) + 3;
+                int lastLine = Mathf.Min(firstLine + shownLineCount, m_objects.Count / ColumnCount + 2);
+
+                for (int i = firstLine; i < lastLine; i++)
+                {
+                    if (i * ColumnCount > m_objects.Count - 1)
+                        break;
+
+                    DrawLine(i * (int)lineHeight, i * ColumnCount, r.width, lineHeight);
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+        GUILayout.EndScrollView();
+        GUILayout.EndArea();
+    }
+
+    public void SetSortParams(int sortSlot, bool descending)
+    {
+        //_sortSlot = sortSlot;
+        _descending = descending;
+    }
+
+    public void SetSelected(object obj)
+    {
+        m_selected = obj;
+
+        if (OnSelected != null)
+            OnSelected(obj, 0);
+    }
+
+    public object GetSelected()
+    {
+        return m_selected;
+    }
+}
---
+++ Common/Editor/TileView/TileView_Impl.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+
+public partial class TileView
+{
+    //private void DrawTitle(float width)
+    //{
+    //    for (int i = 0; i < m_descArray.Count; i++)
+    //    {
+    //        var desc = m_descArray[i];
+
+    //        Rect r = LabelRect(width, i, 0);
+    //        bool selected = _sortSlot == i;
+    //        GUI.Label(r, desc.TitleText + (selected ? _appearance.GetSortMark(_descending) : ""), _appearance.GetTitleStyle(selected));
+    //        if (Event.current.type == EventType.MouseDown && r.Contains(Event.current.mousePosition))
+    //        {
+    //            if (_sortSlot == i)
+    //            {
+    //                _descending = !_descending;
+    //            }
+    //            else
+    //            {
+    //                _sortSlot = i;
+    //            }
+
+    //            //SortData();
+    //            m_hostWindow.Repaint();
+    //        }
+    //    }
+    //}
+
+    private void DrawLine(int startY, int firstObjIndex, float width, float lineHeight)
+    {
+        GUIStyle style = new GUIStyle(((startY / (int)lineHeight) % 2 != 0) ? _appearance.Style_Line : _appearance.Style_LineAlt);
+
+        for (int i = 0; i < ColumnCount; i++)
+        {
+            if (firstObjIndex + i > m_objects.Count - 1)
+                break;
+
+            DrawLineCol(startY, i, width, firstObjIndex + i, style, lineHeight);
+        }
+    }
+
+    private void DrawLineCol(int startY, int col, float width, int objIndex, GUIStyle style, float lineHeight)
+    {
+        object obj = m_objects[objIndex];
+
+        float cellWidth = width / ColumnCount;
+        var rect = new Rect(cellWidth * col, startY, cellWidth, lineHeight);
+
+        if (rect.Contains(Event.current.mousePosition))
+        {            
+            if (Event.current.type == EventType.MouseDown && m_selectedIndex != objIndex)
+            {
+                m_selectedIndex = objIndex;
+                if (OnSelected != null)
+                    OnSelected(obj, objIndex);
+            }
+            m_hostWindow.Repaint();
+        }
+
+        TextureInfoItem item = obj as TextureInfoItem;
+        if (item != null)
+        {
+            GUI.DrawTexture(rect, item.TextureObject);
+
+            if (m_selectedIndex == objIndex /*&& m_selected == obj*/)
+            {
+                style = _appearance.Style_SelectedCell;
+            }
+
+            Rect textRect = rect;
+            textRect.y = rect.y + rect.height - 20;
+            textRect.height = 20;
+            style.alignment = TextAnchor.LowerLeft;
+
+            string text = string.Format("{0}. {1} ({2}x{3})", 
+                objIndex, item.TextureName, item.TextureWidth, item.TextureHeight);
+
+            GUI.Label(textRect, new GUIContent(text, text), style);
+        }
+    }
+
+    //private void SortData()
+    //{
+    //    m_objects.Sort((s1, s2) =>
+    //    {
+    //        if (_sortSlot >= m_descArray.Count)
+    //            return 0;
+
+    //        return m_descArray[_sortSlot].Compare(s1, s2) * (_descending ? -1 : 1);
+    //    });
+    //}
+
+    TileViewAppr _appearance = new TileViewAppr();
+
+    Vector2 _scrollPos = Vector2.zero;
+
+    bool _descending = true;
+    public bool Descending
+    {
+        get { return _descending; }
+        set
+        {
+            _descending = value;
+        }
+    }
+    EditorWindow m_hostWindow = null;
+    List<object> m_objects = new List<object>();
+
+    object m_selected = null;
+    int m_selectedIndex = -1;
+
+    Dictionary<object, Color> m_specialTextColors;
+}
