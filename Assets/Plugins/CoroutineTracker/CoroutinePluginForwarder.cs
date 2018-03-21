0a6c5de4ac57e29b6e3f998683e84785dd18e18e
gulu_office <gulu-dev@outlook.com>
2018-03-21 11:04:10 +0800

    add Plugins folder; move sources into PerfAssist folder

---
+++ Assets/Plugins/CoroutineTracker/CoroutinePluginForwarder.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate Coroutine CoroutineStartHandler_IEnumerator(MonoBehaviour initiator, IEnumerator routine);
+public delegate Coroutine CoroutineStartHandler_String(MonoBehaviour initiator, string methodName, object arg = null);
+
+public class CoroutinePluginForwarder
+{
+    public static CoroutineStartHandler_IEnumerator InvokeStart_IEnumerator;
+    public static CoroutineStartHandler_String InvokeStart_String;
+
+    public static Coroutine InvokeStart(MonoBehaviour initiator, IEnumerator routine)
+    {
+        if (InvokeStart_String != null)
+        {
+            return InvokeStart_IEnumerator(initiator, routine);
+        }
+        else
+        {
+            return initiator.StartCoroutine(routine);
+        }
+    }
+
+    public static Coroutine InvokeStart(MonoBehaviour initiator, string methodName, object arg = null)
+    {
+        if (InvokeStart_String != null)
+        {
+            return InvokeStart_String(initiator, methodName, arg);
+        }
+        else
+        {
+            return initiator.StartCoroutine(methodName, arg);
+        }
+    }
+}
+
---
+++ Assets/Plugins/CoroutineTrackerDemo/TestPluginRunner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPluginRunner : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+        CoroutinePluginForwarder.InvokeStart(this, Co01_Plugin_NULL());
+        CoroutinePluginForwarder.InvokeStart(this, "Co02_Plugin_EOF_ByName");
+        CoroutinePluginForwarder.InvokeStart(this, "Co03_Plugin_EOF_ByNameWithArg", 0.369f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public IEnumerator Co01_Plugin_NULL()
+    {
+        while (true)
+        {
+            Debug.LogFormat("Co01_Plugin_NULL.tick: {0}", Time.time);
+            yield return null;
+        }
+    }
+    public IEnumerator Co02_Plugin_EOF_ByName()
+    {
+        while (true)
+        {
+            Debug.LogFormat("Co02_Plugin_EOF_ByName.tick: {0}", Time.time);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    public IEnumerator Co03_Plugin_EOF_ByNameWithArg(float arg)
+    {
+        while (true)
+        {
+            Debug.LogFormat("Co03_Plugin_EOF_ByNameWithArg.tick: {0} - {1}", Time.time, arg);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
